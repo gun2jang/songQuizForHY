be0736546e476f77c95615cd421212c91ff3f895
gun2jang <firstunderbar@gmail.com>
2021-11-04 18:25:50 +0900

    6:25r

--- routers/room.js
+++ routers/room.js
@@ -3,15 +3,38 @@ const router = require('express').Router();
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const songList = [];
+const roomCapacity = 3;
+
+// for(let i = 0;i<roomCapacity;i++){
+//   io.of();
+// }
 
 router.get('/room/:roomNum', (req, res) => {
   res.render()
   io.on('connection', (socket) => {
     username = req.session.username;
-      socket.on('chat message', msg, username => {
-        io.emit('chat message', msg, username);
-      });
+
+    socket.on('ready', () => {
+      io.
+    })
+    socket.on('gamestart', () => {
+      io.emit('isReady')
+    })
+
+    socket.on('answer', (msg, username) => {
+      if (songList[stageNum].answer == msg) {
+        io.in.emit('correct', (msg, username) => {
+
+        })
+      } else {
+        io.emit('wrong', ()=>{})
+      }
+    });
   });
+  io.on('disconnection', (socket) => {
+    // some code
+  })
 })
 
 module.exports = router;
